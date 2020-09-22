289ab453e98e4a98728cc72b60defe6131b24009
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-09-22 13:39:01 +0700

    added leopays

---
+++ modules/action_data/leopays-newaccount.js
@@ -0,0 +1,26 @@
+const hyperionModule = {
+    chain: "*",
+    contract: 'lpc',
+    action: 'newaccount',
+    parser_version: ['1.8', '1.7'],
+    handler: (action) => {
+        // attach action extras here
+        let name = null;
+        const data = action['act']['data'];
+        if (data['newact']) {
+            name = data['newact'];
+        } else if (data['name']) {
+            name = data['name'];
+            delete data['name'];
+        }
+        if (name) {
+            action['@newaccount'] = {
+                active: data['active'],
+                owner: data['owner'],
+                newact: name
+            }
+        }
+    }
+};
+
+module.exports = { hyperionModule };
---
+++ modules/action_data/leopays-updateauth.js
@@ -0,0 +1,21 @@
+const hyperionModule = {
+    chain: "*",
+    contract: 'lpc',
+    action: 'updateauth',
+    parser_version: ['1.8', '1.7'],
+    handler: (action) => {
+        // attach action extras here
+        const data = action['act']['data'];
+        const _auth = data['auth'];
+        if (_auth['accounts'].length === 0) delete _auth['accounts'];
+        if (_auth['keys'].length === 0) delete _auth['keys'];
+        if (_auth['waits'].length === 0) delete _auth['waits'];
+        action['@updateauth'] = {
+            permission: data['permission'],
+            parent: data['parent'],
+            auth: _auth
+        };
+    }
+};
+
+module.exports = { hyperionModule };
